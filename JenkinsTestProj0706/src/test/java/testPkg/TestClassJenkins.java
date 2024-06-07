04d0b0e74f77ac900bc58fd6dee9feee751aa3a0
rushm <rushm@LAPTOP-DENRBFPO.mshome.net>
2024-06-07 10:19:33 +0530

    Jenkins project

---
+++ JenkinsTestProj0706/src/test/java/testPkg/TestClassJenkins.java
@@ -0,0 +1,11 @@
+package testPkg;
+
+import org.testng.annotations.Test;
+
+public class TestClassJenkins {
+@Test
+public void testMethodJenkins()
+{
+	System.out.println("Jenkins execution");
+}
+}
